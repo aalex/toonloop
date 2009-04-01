67a4b41865d18427e8dd5869299616016bd0bcec
quessy <devnull@localhost>
2009-04-01 19:31:32 -0400

    [svn r155] deep cleanup

--- toon/__init__.py
+++ toon/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+# 
 # ToonLoop
 # Copyright (C) 2009 Alexandre Quessy 
 # http://alexandre.quessy.net
@@ -26,11 +26,23 @@ See https://svn.sat.qc.ca/trac/rawmaterials
 """
 
 __version__ = '1.0 alpha'
-
 import osc_protocol
+__all__ = ("osc_protocol")
 
-__all__ = ("osc_protocol", "utils")
+def osc_create_and_send(osc, host, addr="", msg=None, typehint=None):
+    """
+    Creates and send and OSC message.
+    
+    :param osc: is a OSC twisted server/client instance. see osc_protocol.py
+    :param host: is a tuple (ip, port)
+    :param addr: is the 4th arg to OSC callbacks
+    :param msg: can be a list
 
-from utils import osc_create_and_send
-
+    Example:
+    osc_create_and_send(self.osc,host,'/foo/bar', [1,2,'egg','spam'])
+    """
+    m = osc_protocol.OscMessage(addr)
+    if msg is not None:
+        m.append(msg, typehint)
+    osc.send_message(host[0], host[1], m)
 
