919d14d362096e17af004bf9c693298a61a5367d
quessy <devnull@localhost>
2009-04-02 16:35:49 -0400

    [svn r162] adding teapot glsl example

--- prototypes/glsl/keying.frag.glsl
+++ prototypes/glsl/keying.frag.glsl
@@ -1,15 +1,9 @@
 /**
- * Fragment shader for keying
+ * Fragment shader for keying. (using a green or blue screen)
  * 
  * user-configurable variables (read-only)
  * main thing is, make sure the texcoord's arnt normalized (so they are in the range of [0..w, 0..h] )
  */
-// TODO : uniform float thresh;
-/*
-uniform float green_gt;
-uniform float red_gt;
-uniform float blue_gt; 
-*/
 
 uniform vec3 keying_color;
 uniform vec3 thresh; // how close to this color a pixels disapears.
@@ -34,12 +28,7 @@ void main(void)
 	{
 	   output_alpha = 0.0;
 	}
-	
-    // if (color.g >= 0.5 && color.b <= 0.5 && color.r <= 0.5) 
-    // {
-    //     output_alpha = 0.0;
-    // }
-    // gl_FragColor = alpha * vec4(color, alpha);
     
     gl_FragColor = vec4(input_color, output_alpha); 
 }
+
