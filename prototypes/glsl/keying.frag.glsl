2c3e0bd86bbcf4ebc874b3235c2e5f8523a96af5
quessy <devnull@localhost>
2009-04-02 08:34:40 -0400

    [svn r161] keying shader works !

--- prototypes/glsl/keying.frag.glsl
+++ prototypes/glsl/keying.frag.glsl
@@ -11,6 +11,9 @@ uniform float red_gt;
 uniform float blue_gt; 
 */
 
+uniform vec3 keying_color;
+uniform vec3 thresh; // how close to this color a pixels disapears.
+
 uniform sampler2DRect image;
 
 varying vec2 texcoord0;
@@ -18,13 +21,25 @@ varying vec2 texdim0;
 
 void main(void)
 {
-    vec3 color = texture2DRect(image, texcoord0).rgb;
-    float alpha = 1.0;
+    // sample from the texture 
+    vec3 input_color = texture2DRect(image, texcoord0).rgb;
+    float output_alpha = 1.0;
     
-    if (color.g >= 0.5 && color.b <= 0.5 && color.r <= 0.5) 
-    {
-        alpha = 0.0;
-    }
-    //gl_FragColor = vec4(color.r, color.g, 1.0, alpha);
-    gl_FragColor = alpha * vec4(color, alpha); 
+    // measure distance from keying_color
+    vec3 delta = abs(input_color - keying_color);
+	
+	// for now, not visible if under threshold of proximity
+	// TODO: mix() according the 3 factors of proximity.
+	if (delta.r <= thresh.r && delta.g <= thresh.g && delta.b <= thresh.b)
+	{
+	   output_alpha = 0.0;
+	}
+	
+    // if (color.g >= 0.5 && color.b <= 0.5 && color.r <= 0.5) 
+    // {
+    //     output_alpha = 0.0;
+    // }
+    // gl_FragColor = alpha * vec4(color, alpha);
+    
+    gl_FragColor = vec4(input_color, output_alpha); 
 }
