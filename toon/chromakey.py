303d7394b4503a6a5237fdbae0e520c5ceb7d30a
quessy <devnull@localhost>
2009-04-24 02:54:18 -0400

    [svn r278] moved stuff to main

--- toon/chromakey.py
+++ toon/chromakey.py
@@ -2,11 +2,6 @@
 """
 GLSL shaders with SDL, OpenGL texture and Python
 """
-import os
-import sys
-import pygame
-import pygame.image
-import pygame.camera
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
@@ -94,17 +89,6 @@ def set_program_uniforms():
     program.glUniform3f("keying_color", 0.0, 1.0, 0.0)
     program.glUniform3f("thresh", 0.8, 0.5, 0.5)
 
-def resize((width, height)):
-    """
-    Called when we resize the window.
-    (fullscreen on/off)
-    """
-    if height == 0:
-        height = 1
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    glOrtho(-4.0, 4.0, -3.0, 3.0, -1.0, 1.0)
-    glMatrixMode(GL_MODELVIEW)
 
 def program_init():
     global program
@@ -121,145 +105,163 @@ def program_disable():
     global program
     program.disable()
 
-def gl_init():
-    """
-    Init the window
-    """
-    global program 
-    global textures
-
-    glEnable(GL_TEXTURE_RECTANGLE_ARB) 
-    glShadeModel(GL_SMOOTH)
-    textures[0] = glGenTextures(1)
-    glClearColor(0.0, 0.0, 0.0, 0.0)
-    glEnable(GL_BLEND)
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-    glColor3f(1., 1., 1.)
-    program_init()
-
-def draw():
-    """
-    Called on every frame rendering
-    """
-    global program 
-    global textures
-    
-    # w = 1; h = 1
-    w = 640.0
-    h = 480.0
-
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    program.enable()
-    set_program_uniforms()
-    glPushMatrix()
-    glBegin(GL_QUADS)
-    glTexCoord2f(0.0, 0.0)
-    glVertex2f(-4.0, -3.0) # Bottom Left
-    glTexCoord2f(w, 0.0)
-    glVertex2f(4.0, -3.0) # Bottom Right
-    glTexCoord2f(w, h)
-    glVertex2f(4.0, 3.0) # Top Right
-    glTexCoord2f(0.0, h)
-    glVertex2f(-4.0, 3.0) # Top Left
-    glEnd()
-    glPopMatrix()
-
-    program.disable()
-    glPushMatrix()
-    glTranslate(2, 1.5, 0)
-    glBegin(GL_QUADS)
-    glTexCoord2f(0.0, 0.0)
-    glVertex2f(-1.333, -1.) # Bottom Left
-    glTexCoord2f(w, 0.0)
-    glVertex2f(1.333, -1.) # Bottom Right
-    glTexCoord2f(w, h)
-    glVertex2f(1.333, 1.) # Top Right
-    glTexCoord2f(0.0, h)
-    glVertex2f(-1.333, 1.) # Top Left
-    glEnd()
-    glPopMatrix()
-
-# ----------------------------  classes  ----------------------------
-class VideoCapturePlayer(object):
-    size = (640, 480)
-    def __init__(self, **argd):
-        self.__dict__.update(**argd)
-        super(VideoCapturePlayer, self).__init__(**argd)
-
-        # create a display surface. standard pygame stuff
-        self.screen = pygame.display.set_mode(self.size, OPENGL | DOUBLEBUF | HWSURFACE)
-        pygame.display.set_caption("GLCamera")
-        resize(self.size)
-        gl_init()
-
-        # gets a list of available cameras.
-        self.clist = pygame.camera.list_cameras()
-        if not self.clist:
-            raise ValueError("Sorry, no cameras detected.")
-        
-        if os.uname()[0] == 'Darwin':
-            self.isMac = True
-            # creates the camera of the specified size and in RGB colorspace
-            self.camera = pygame.camera.Camera(0, self.size, 'RGBA')
-        else:
-            self.isMac = False
-            # creates the camera of the specified size and in RGB colorspace
-            self.camera = pygame.camera.Camera('/dev/video0', self.size, "RGBA")
-
-    
-        # starts the camera
-        self.camera.start()
-        self.clock = pygame.time.Clock()
-
-        # create a surface to capture to.  for performance purposes, you want the
-        # bit depth to be the same as that of the display surface.
-        self.snapshot = pygame.surface.Surface(self.size, 0, self.screen)
-
-    def get_and_flip(self):
-        """
-        Grabs a frame from the camera (to a texture) and renders the screen.
-
-        if you don't want to tie the framerate to the camera, you can check and
-        see if the camera has an image ready.  note that while this works
-        on most cameras, some will never return true.
-        """
-        global textures
-
-        if 0 and self.camera.query_image():
-            # capture an image
-            self.snapshot = self.camera.get_image(self.snapshot)
-        
-        self.snapshot = self.camera.get_image(self.snapshot)
-        textureData = pygame.image.tostring(self.snapshot, "RGBX", 1)
-    
-        if bool(glActiveTexture):   # make sure glActiveTexture is defined
-            glActiveTexture(GL_TEXTURE0) # IMPORTANT ! sets the texture unit to 0. 
-
-        glBindTexture(GL_TEXTURE_RECTANGLE_ARB, textures[0]) # GL_TEXTURE_2D
-        glTexImage2D(GL_TEXTURE_RECTANGLE_ARB, 0, GL_RGBA, self.snapshot.get_width(), self.snapshot.get_height(), 0,
-                  GL_RGBA, GL_UNSIGNED_BYTE, textureData )
-        glTexParameterf(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MAG_FILTER, GL_NEAREST) # GL_TEXTURE_RECTANGLE_ARB
-        glTexParameterf(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-        draw()
-        pygame.display.flip()
-    
-    def main(self):
-        going = True
-        while going:
-            events = pygame.event.get()
-            for e in events:
-                if e.type == QUIT or (e.type == KEYDOWN and e.key == K_ESCAPE):
-                    going = False
-                elif e.type == KEYDOWN:
-                    if e.key == K_f:
-                        pygame.display.toggle_fullscreen()
-
-            self.get_and_flip()
-            self.clock.tick(30)
-            #print "FPS:", self.clock.get_fps()
 
 # ----------------------------  main  ----------------------------
 if __name__ == '__main__':
+    import os
+    import sys
+    import pygame
+    import pygame.image
+    import pygame.camera
+
+    def resize((width, height)):
+        """
+        Called when we resize the window.
+        (fullscreen on/off)
+        """
+        if height == 0:
+            height = 1
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glOrtho(-4.0, 4.0, -3.0, 3.0, -1.0, 1.0)
+        glMatrixMode(GL_MODELVIEW)
+
+    def gl_init():
+        """
+        Init the window
+        """
+        global program 
+        global textures
+
+        glEnable(GL_TEXTURE_RECTANGLE_ARB) 
+        glShadeModel(GL_SMOOTH)
+        textures[0] = glGenTextures(1)
+        glClearColor(0.0, 0.0, 0.0, 0.0)
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        glColor3f(1., 1., 1.)
+        program_init()
+
+    def draw():
+        """
+        Called on every frame rendering
+        """
+        global program 
+        global textures
+        
+        # w = 1; h = 1
+        w = 640.0
+        h = 480.0
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        program.enable()
+        set_program_uniforms()
+        glPushMatrix()
+        glBegin(GL_QUADS)
+        glTexCoord2f(0.0, 0.0)
+        glVertex2f(-4.0, -3.0) # Bottom Left
+        glTexCoord2f(w, 0.0)
+        glVertex2f(4.0, -3.0) # Bottom Right
+        glTexCoord2f(w, h)
+        glVertex2f(4.0, 3.0) # Top Right
+        glTexCoord2f(0.0, h)
+        glVertex2f(-4.0, 3.0) # Top Left
+        glEnd()
+        glPopMatrix()
+
+        program.disable()
+        glPushMatrix()
+        glTranslate(2, 1.5, 0)
+        glBegin(GL_QUADS)
+        glTexCoord2f(0.0, 0.0)
+        glVertex2f(-1.333, -1.) # Bottom Left
+        glTexCoord2f(w, 0.0)
+        glVertex2f(1.333, -1.) # Bottom Right
+        glTexCoord2f(w, h)
+        glVertex2f(1.333, 1.) # Top Right
+        glTexCoord2f(0.0, h)
+        glVertex2f(-1.333, 1.) # Top Left
+        glEnd()
+        glPopMatrix()
+
+    # ----------------------------  classes  ----------------------------
+    class VideoCapturePlayer(object):
+        size = (640, 480)
+        def __init__(self, **argd):
+            self.__dict__.update(**argd)
+            super(VideoCapturePlayer, self).__init__(**argd)
+
+            # create a display surface. standard pygame stuff
+            self.screen = pygame.display.set_mode(self.size, OPENGL | DOUBLEBUF | HWSURFACE)
+            pygame.display.set_caption("GLCamera")
+            resize(self.size)
+            gl_init()
+
+            # gets a list of available cameras.
+            self.clist = pygame.camera.list_cameras()
+            if not self.clist:
+                raise ValueError("Sorry, no cameras detected.")
+            
+            if os.uname()[0] == 'Darwin':
+                self.isMac = True
+                # creates the camera of the specified size and in RGB colorspace
+                self.camera = pygame.camera.Camera(0, self.size, 'RGBA')
+            else:
+                self.isMac = False
+                # creates the camera of the specified size and in RGB colorspace
+                self.camera = pygame.camera.Camera('/dev/video0', self.size, "RGBA")
+
+        
+            # starts the camera
+            self.camera.start()
+            self.clock = pygame.time.Clock()
+
+            # create a surface to capture to.  for performance purposes, you want the
+            # bit depth to be the same as that of the display surface.
+            self.snapshot = pygame.surface.Surface(self.size, 0, self.screen)
+
+        def get_and_flip(self):
+            """
+            Grabs a frame from the camera (to a texture) and renders the screen.
+
+            if you don't want to tie the framerate to the camera, you can check and
+            see if the camera has an image ready.  note that while this works
+            on most cameras, some will never return true.
+            """
+            global textures
+
+            if 0 and self.camera.query_image():
+                # capture an image
+                self.snapshot = self.camera.get_image(self.snapshot)
+            
+            self.snapshot = self.camera.get_image(self.snapshot)
+            textureData = pygame.image.tostring(self.snapshot, "RGBX", 1)
+        
+            if bool(glActiveTexture):   # make sure glActiveTexture is defined
+                glActiveTexture(GL_TEXTURE0) # IMPORTANT ! sets the texture unit to 0. 
+
+            glBindTexture(GL_TEXTURE_RECTANGLE_ARB, textures[0]) # GL_TEXTURE_2D
+            glTexImage2D(GL_TEXTURE_RECTANGLE_ARB, 0, GL_RGBA, self.snapshot.get_width(), self.snapshot.get_height(), 0,
+                      GL_RGBA, GL_UNSIGNED_BYTE, textureData )
+            glTexParameterf(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MAG_FILTER, GL_NEAREST) # GL_TEXTURE_RECTANGLE_ARB
+            glTexParameterf(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+            draw()
+            pygame.display.flip()
+        
+        def main(self):
+            going = True
+            while going:
+                events = pygame.event.get()
+                for e in events:
+                    if e.type == QUIT or (e.type == KEYDOWN and e.key == K_ESCAPE):
+                        going = False
+                    elif e.type == KEYDOWN:
+                        if e.key == K_f:
+                            pygame.display.toggle_fullscreen()
+
+                self.get_and_flip()
+                self.clock.tick(30)
+                #print "FPS:", self.clock.get_fps()
     pygame.init()
     pygame.camera.init()
     VideoCapturePlayer().main()
