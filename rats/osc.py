9b1f885945972bc88964c2a26d9e732294f7cc15
quessy <devnull@localhost>
2009-08-02 20:33:47 -0400

    [svn r374] added license info to rats/osc.py

--- rats/osc.py
+++ rats/osc.py
@@ -1,5 +1,33 @@
 #!/usr/bin/env python
+#
+# ToonLoop for Python
+#
+# Copyright 2008 Alexandre Quessy & Tristan Matthews
+# <alexandre@quessy.net> & <le.businessman@gmail.com>
+# http://www.toonloop.com
+#
+# Original idea by Alexandre Quessy
+# http://alexandre.quessy.net
+#
+# ToonLoop is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ToonLoop is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the gnu general public license
+# along with ToonLoop.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+Implementation of the OSC protocol for Twisted. 
 
+Server and clients use the pyliblo library to integrate well with a twisted 
+asynchronous networking application. 
+"""
 import liblo
 import sys
 import time
@@ -127,6 +155,7 @@ class OscServer(object):
             self._delayed_id = reactor.callLater(self.interval, self._poll)
 
 if __name__ == "__main__":
+    # examples : 
     def foo_bar_callback(path, args):
         i, f = args
         print "received message '%s' with arguments '%d' and '%f'" % (path, i, f)
