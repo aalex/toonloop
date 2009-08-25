e9491b8b31f776a3f52a0c40213bc8f8f435c317
quessy <devnull@localhost>
2009-08-25 09:09:44 -0400

    [svn r389] improved pure mess

--- rats/purepy/obj.py
+++ rats/purepy/obj.py
@@ -21,15 +21,17 @@
 
 import random 
 from rats import fudi
+from zope.interface import implements # TODO
 
 class Obj(object):
     """
     Generic Pure Data Object.
     """
+    # TODO implements IElement
     def __init__(self, name, *args, **keywords):
         self.name = name
         self.args = args
-        self.subpatch_name = "default"
+        #self.subpatch_name = "default"
         self.pos = [random.randrange(10, 600), random.randrange(10, 400)]
         if keywords.has_key("pos"):
             self.pos = keywords["pos"]
@@ -38,7 +40,8 @@ class Obj(object):
         """ 
         Converts to list suitable for FUDI creation message. 
         """
-        li = [self.subpatch_name, "obj", self.pos[0], self.pos[1], self.name]
+        # self.subpatch_name, 
+        li = ["obj", self.pos[0], self.pos[1], self.name]
         li.extend(self.args)
         return li
 
@@ -46,14 +49,16 @@ class Receive(Obj):
     """
     The [receive] Pure Data object.
     """
+    # TODO implements IElement
     def __init__(self, receive_symbol):
+        self.receive_symbol = receive_symbol
         Obj.__init__(self, "r", receive_symbol)
 
     def send(self, *args):
         """
         Returns a FUDI message suitable to be sent to that [receive] object. 
         """
-        li = [self.args[0]]
+        li = [self.receive_symbol]
         li.extend(args)
         return li
 
@@ -61,24 +66,26 @@ class Connection(object):
     """
     Connection between two Pure Data objects.
     """
+    # TODO implements IElement
     def __init__(self, from_object, from_outlet, to_object, to_inlet):
         self.from_object = from_object
         self.from_outlet = from_outlet
         self.to_object = to_object
         self.to_inlet = to_inlet
-        self.subpatch_name = "default"
+        # self.subpatch_name = "default"
 
     def get_fudi(self):
         """
         Returns fudi creation list.
         """
-        return [self.subpatch_name, "connect", self.from_object.id, self.from_outlet, self.to_object.id, self.to_inlet]
+        # self.subpatch_name, 
+        return ["connect", self.from_object.id, self.from_outlet, self.to_object.id, self.to_inlet]
 
 class SubPatch(object):
     """
     Pure Data Subpatch. 
     """
-    def __init__(self, name):
+    def __init__(self, name=None):
         self.name = name
         self.objects = []
         self.connections = []
@@ -89,10 +96,25 @@ class SubPatch(object):
         Objects and connections
         """
         li = []
+        print "objects"
         for obj in self.objects:
-            li.append(obj.get_fudi())
+            if self.name is None:
+                l = []
+            else:
+                l = [self.name]
+            l.extend(obj.get_fudi())
+            li.append(l)
+            print l
+        print "connections"
         for conn in self.connections:
-            li.append(conn.get_fudi())
+            if self.name is None:
+                l = []
+            else:
+                l = [self.name]
+            l.extend(conn.get_fudi())
+            li.append(l)
+            print l
+        print "done creating FUDI list"
         return li
 
     def obj(self, name, *args, **keywords):
@@ -102,12 +124,12 @@ class SubPatch(object):
         @return Obj instance.
         """
         obj = Obj(name, *args, **keywords)
-        return self._add(obj)
+        return self._add_object(obj)
 
-    def _add(self, obj):
+    def _add_object(self, obj):
         """ Common to obj() and receive(). """
         obj.id = len(self.objects)
-        obj.subpatch_name = self.name
+        # obj.subpatch_name = self.name
         self.objects.append(obj)
         return obj
         
@@ -117,7 +139,7 @@ class SubPatch(object):
         Appends a [receive] object to the subpatch.
         """
         obj = Receive(receive_symbol)
-        return self._add(obj)
+        return self._add_object(obj)
     
     def connect(self, from_object, from_outlet, to_object, to_inlet):
         """
@@ -131,7 +153,7 @@ class SubPatch(object):
         else:
             conn = Connection(from_object, from_outlet, to_object, to_inlet)
             self.connections.append(conn)
-            conn.subpatch_name = self.name
+            # conn.subpatch_name = self.name
             
     def clear(self):
         """
@@ -143,13 +165,18 @@ class SubPatch(object):
         return ["pd-%s" % (self.name), "clear"]
 
 if __name__ == "__main__":
-    s = SubPatch("hello_01")
-    tgl = s.obj("tgl")
-    metro = s.obj("metro", 50)
-    s.connect(tgl, 0, metro, 0)
-    li = s.get_fudi()
+    sub = SubPatch("hello_01")
+    tgl = sub.obj("tgl")
+    metro = sub.obj("metro", 50)
+    sub.connect(tgl, 0, metro, 0)
+    li = sub.get_fudi()
+    print "result:"
+    print li
     for i in li:
-        print(fudi.to_fudi(*i))
+        if len(i) == 0:
+            print(fudi.to_fudi(i[0]))
+        else:
+            print(fudi.to_fudi(i[0], *i[1:]))
 
 
 
