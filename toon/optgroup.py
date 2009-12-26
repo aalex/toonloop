fd2b1399f46968c3e78471897ad401d427d4da1d
Alexandre Quessy on plouf <alexandre@quessy.net>
2009-12-26 17:16:46 -0500

    Towards formal optionsgroups in fx.  * Create the OptionsGroup class.  * TelnetController now parses lines with shlex. Ready to send it to optgroups

--- toon/optgroup.py
+++ toon/optgroup.py
@@ -4,59 +4,58 @@ Groups of options set on the command line.
 """
 VERBOSE = False
 
-class UsageError(Exception):
+class OptionsError(Exception):
     """
-    Any error caused by a bad usage of the command-line.
+    Any error related to OptionsGroup
     """
     pass
 
-def parse_options_groups(groups, groups_options):
+class OptionsGroup(object):
     """
-    Changes attributes of objects passed in the groups dict according to the 
-    groups_options optparse parsed option.
-
-    Attributes that are lists are string whose elements are separated by commas. (",")
-
-    :param groups: dict of groups objects whose keys are their names
-    :param groups_options: optparse option with action="append" and nargs=3
+    Its attributes are the its option names and values.
     """
-    try:
-        for group, key, value in groups_options:
-            if VERBOSE:
-                print("    group:%s, k:%s, v:%s" % (group, key, value))
-            if group in groups.keys():
-                obj = groups[group]
-                if hasattr(obj, key):
-                    attr = getattr(obj, key)
-                    attr_type = type(attr)
-                    if attr_type in [int, float, str]:
-                        try:
-                            setattr(obj, key, attr_type(value))
-                        except ValueError, e:
-                            raise UsageError("Could not convert value %s to type %s for option %s of group %s. %s" % (value, attr_type.__name__, key, group, e.message))
-                    elif attr_type is list:
-                        tuple_length = len(attr)
-                        elements = value.split(",")
-                        if len(elements) != tuple_length:
-                            raise UsageError("Option %s of group %s should have %s elements but has %s." % (key, group, tuple_length, len(elements)))
-                        else:
-                            for i in range(len(elements)):
-                                element_type = type(attr[i])
-                                try:
-                                    attr[i] = element_type(elements[i])
-                                except ValueError, e:
-                                    raise UsageError("Could not convert element %s (%s) to type %s for option %s of group %s. %s" % (i, elements[i], element_type.__name__, key, group, e.message))
-                    else:
-                        raise Usage("Impossible to convert option %s of group %s to type %s." % (key, group, attr_type))
+    def set_value(self, option_name, value):
+        """
+        Sets a value of an option. 
+        
+        If value is a string, it will be casted to the type of the option
+        to be set. If the option is a tuple of 4 floats, for example, it 
+        will be expected to in the form "1.0,0.8,0.2,1.0" where "," is the 
+        separator.
+
+        :param option_name: Name of the attribute
+        :param value: Either a string, or any Python type. 
+        """
+        if not hasattr(self, key):
+            raise OptionsError("No no option named %s in options group" % (key))
+        else:
+            attr = getattr(self, key)
+            attr_type = type(attr)
+            if attr_type in [int, float, str]:
+                try:
+                    setattr(self, key, attr_type(value))
+                except ValueError, e:
+                    raise OptionsError("Could not convert value %s to type %s for option %s. %s" % (value, attr_type.__name__, key, e.message))
+            elif attr_type is list:
+                tuple_length = len(attr)
+                if type(value) is str:
+                    elements = value.split(",")
                 else:
-                    raise UsageError("No no option named %s in options group %s" % (key, group))
+                    elements = value # we assume it is a list
+                if len(elements) != tuple_length:
+                    raise OptionsError("Option %s should have %s elements but has %s." % (key, tuple_length, len(elements)))
+                else:
+                    for i in range(len(elements)):
+                        element_type = type(attr[i])
+                        try:
+                            attr[i] = element_type(elements[i])
+                        except ValueError, e:
+                            raise OptionsError("Could not convert element %s (%s) to type %s for option %s. %s" % (i, elements[i], element_type.__name__, key, e.message))
             else:
-                raise UsageError("No options group named %s" % (group))
-    except UsageError, e:
-        print("ERROR: %s" % (e.message))
+                raise OptionsError("Impossible to convert option %s of group %s to type %s." % (key, group, attr_type))
 
 if __name__ == "__main__":
-    class ExampleGroup(object):
+    class ExampleGroup(OptionsGroup):
         def __init__(self):
             self.i = 3
             self.f = 2.0
@@ -77,8 +76,15 @@ if __name__ == "__main__":
         print("    %s: %s" % (name, group.__dict__))
     print("effect_option: %s" % (options.effect_option))
     if options.effect_option is not None:
-        parse_options_groups(groups, options.effect_option)
+        for group, key, value in options.effect_option:
+            #if VERBOSE:
+            #    print("    group:%s, k:%s, v:%s" % (group, key, value))
+            if group not in groups.keys():
+                print("No group named %s." % (group))
+            else:
+                obj = groups[group]
+                obj.set_value(key, value)
+        #parse_options_groups(groups, options.effect_option)
         print("After:")
         for name, group in groups.items():
             print("%s: %s" % (name, group.__dict__))
-
