61e6a392f38606209067bdd8e039099618dd3bb0
Alexandre Quessy <alexandre@quessy.net>
2010-02-18 04:02:09 -0500

    something is working with the image numbers

--- tests/check_classes.cpp
+++ tests/check_classes.cpp
@@ -1,41 +1,84 @@
 #include <iostream>
 #include <assert.h>
 #include <list>
-
+/**
+ * A clip is a list of frame numbers.
+ * It can be played and edited.
+ */
 class Clip {
     private:
-        std::list<int> images_;
+        /** List of int. (file names) */
+        std::list<unsigned int> images_;
+        /** Writehead range: [0, n] The next frame index to write. */
+        std::list<unsigned int>::iterator writehead_;
+        /** Playhead range: [0, n-1] The next frame index to play. */
+        std::list<unsigned int>::iterator playhead_;
+        enum PlayheadDirection {
+            BACKWARD, 
+            FORWARD, 
+            BACK_AND_FORTH
+        } direction_;
     public:
         Clip() 
         {
-            images_ = std::list<int>();
+            images_ = std::list<unsigned int>();
+            writehead_ = images_.begin();
+            playhead_ = images_.begin();
             std::cout << "Clip created" << std::endl;
         }
         ~Clip() 
         {
             std::cout << "Clip destroyed" << std::endl;
         }
-        void add_image(int i)
+        /** Adds an image before writehead index. */
+        void add_image()
         {
-            std::cout << "Adding image " << i << std::endl; 
-            images_.push_back(i); // append
+            unsigned int image_number = 999; // TODO: create allocator.
+            std::cout << "Adding image " << image_number << std::endl; 
+            images_.push_back(image_number); 
+            //images_.insert(writehead_, image_number);
+            ++writehead_;
         }
+        /** Removes an image at writehead index. */
         void remove_image()
         {
             if (! images_.empty())
             {
                 images_.pop_back(); // pop
+                //TODO: images_.erase(writehead_);
                 std::cout << "Removing image " << std::endl; 
             } else {
                 std::cout << "Not enough images to pop one." << std::endl; 
             }
+        }
+        void writehead_next()
+        {
+
+        }
+        void writehead_previous()
+        {
+
+        }
+        void writehead_goto(unsigned int index)
+        {
+
+        }
+        /** Go to last image */
+        void writehead_end()
+        {
+            writehead_ = images_.end();
+        }
+        /** Go to first image */
+        void writehead_beginning()
+        {
+            writehead_ = images_.begin();
         }
 };
 
 int main(int argc, char *argv[])
 {
     Clip clip = Clip();
-    clip.add_image(1);
+    clip.add_image();
     clip.remove_image();
     assert(1 == 1);
     return 0;
