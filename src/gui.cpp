c906e35dd735784525a35c85d5a79996cb1029eb
Alexandre Quessy <alexandre@quessy.net>
2010-02-22 22:24:45 -0500

    towards a glimagesink

--- src/gui.cpp
+++ src/gui.cpp
@@ -10,22 +10,20 @@
 #include <gdk/gdkkeysyms.h>
 
 #include "gltools.h"
+#include "pipeline.h"
 #include "draw.h"
 
 class Gui
 {
     public:
-        Gui(); // TODO: add pipeline argument
+        GtkWidget *drawing_area_;
+        Gui(); 
         ~Gui() {};
         void toggleFullscreen() { toggleFullscreen(window_); } // no argument version of the same method below.
 
     private:
         GtkWidget *window_;
-        GtkWidget *drawing_area_;
         GLXContext glx_context_;
-        static void on_realize(GtkWidget *widget, gpointer data);
-        static gboolean on_configure_event(GtkWidget *widget, GdkEventConfigure *event, gpointer data);
-        static gboolean on_expose_event(GtkWidget *widget, GdkEventExpose *event, gpointer data);
         static void on_delete_event(GtkWidget* widget, GdkEvent* event, gpointer data);
         static gboolean key_press_event(GtkWidget *widget, GdkEventKey *event, gpointer data);
 
@@ -68,120 +66,6 @@ void Gui::showCursor()
 {
     /// sets to default
     gdk_window_set_cursor(GDK_WINDOW(drawing_area_->window), NULL);
-}
-
-
-/**
- * Sets up the orthographic projection.
- * 
- * Makes sure height is always 1.0 in GL modelview coordinates.
- * 
- * Coordinates should give a rendering area height of 1
- * and a width of 1.33, when in 4:3 ratio.
-*/
-void _set_view(float ratio)
-{
-    float w = ratio;
-    float h = 1.0;
-
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(-w, w, -h, h, -1.0, 1.0);
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-}
-/**
- * Handles the "realize" signal to take any necessary actions when the widget is instantiated on a particular display. (Create GDK resources in response to this signal.) 
- */
-void Gui::on_realize(GtkWidget *widget, gpointer data)
-{
-
-    Gui *context = static_cast<Gui*>(data);
-
-    GdkGLContext *glcontext = gtk_widget_get_gl_context(widget);
-    GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable (widget);
-  
-    /*** OpenGL BEGIN ***/
-    if (! gdk_gl_drawable_gl_begin(gldrawable, glcontext))
-    {
-        return;
-    }
-    context->glx_context_ = glXGetCurrentContext();
-    glDisable(GL_DEPTH_TEST);
-  
-    glClearColor(0.0, 0.0, 0.0, 1.0); // black background
-    glViewport(0, 0, widget->allocation.width, widget->allocation.height);
-    _set_view(widget->allocation.width / float(widget->allocation.height));
-    gdk_gl_drawable_gl_end(gldrawable);
-    /*** OpenGL END ***/
-}
-/**
- * Handles the "configure_event" signal to take any necessary actions when the widget changes size. 
- */
-gboolean Gui::on_configure_event(GtkWidget *widget, GdkEventConfigure *event, gpointer data)
-{
-    Gui *context = static_cast<Gui*>(data);
-    GdkGLContext *glcontext = gtk_widget_get_gl_context(widget);
-    GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable(widget);
-    /*** OpenGL BEGIN ***/
-    if (! gdk_gl_drawable_gl_begin(gldrawable, glcontext))
-    {
-        return FALSE;
-    }
-    glViewport(0, 0, widget->allocation.width, widget->allocation.height);
-    _set_view(widget->allocation.width / float(widget->allocation.height));
-    gdk_gl_drawable_gl_end(gldrawable);
-    /*** OpenGL END ***/
-    return TRUE;
-}
-
-// draws the stuff
-void _draw()
-{
-    // DRAW STUFF HERE
-    glDisable(GL_TEXTURE_RECTANGLE_ARB);
-    glColor4f(1.0, 0.8, 0.2, 1.0);
-    glPushMatrix();
-    glScalef(0.5, 0.5, 1.0);
-    draw::draw_square();
-    glPopMatrix();
-
-    glColor4f(1.0, 1.0, 0.0, 0.8);
-    int num = 64;
-    float x;
-    for (int i = 0; i < num; i++)
-    {
-        x = (i / float(num)) * 4 - 2;
-        draw::draw_line(float(x), -2.0, float(x), 2.0);
-        draw::draw_line(-2.0, float(x), 2.0, float(x));
-    }
-}
-/**
- * Handles the "expose_event" signal to redraw the contents of the widget.
- */
-gboolean Gui::on_expose_event(GtkWidget *widget, GdkEventExpose *event, gpointer data)
-{
-    GdkGLContext *glcontext = gtk_widget_get_gl_context(widget);
-    GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable (widget);
-    /*** OpenGL BEGIN ***/
-    if (!gdk_gl_drawable_gl_begin(gldrawable, glcontext))
-    {
-      return FALSE;
-    }
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    _draw();
-
-    if (gdk_gl_drawable_is_double_buffered(gldrawable))
-    {
-        gdk_gl_drawable_swap_buffers(gldrawable);
-    } else {
-        glFlush();
-    }
-    gdk_gl_drawable_gl_end(gldrawable);
-    /*** OpenGL END ***/
-    return TRUE;
 }
 
 gboolean Gui::key_press_event(GtkWidget *widget, GdkEventKey *event, gpointer data)
@@ -239,29 +123,29 @@ Gui::Gui() :
     isFullscreen_(false)
 {
 
-    glx_context_ = NULL;
-    gint major; 
-    gint minor;
-    gdk_gl_query_version(&major, &minor);
-    g_print("\nOpenGL extension version - %d.%d\n", major, minor);
-    /* Try double-buffered visual */
+    //glx_context_ = NULL;
+    //gint major; 
+    //gint minor;
+    //gdk_gl_query_version(&major, &minor);
+    //g_print("\nOpenGL extension version - %d.%d\n", major, minor);
+    ///* Try double-buffered visual */
 
-    GdkGLConfig* glconfig;
-    // the line above does not work in C++ if the cast is not there.
-    glconfig = gdk_gl_config_new_by_mode(static_cast<GdkGLConfigMode>(GDK_GL_MODE_RGB | GDK_GL_MODE_DOUBLE));
-    if (glconfig == NULL)
-    {
-        g_print("*** Cannot find the double-buffered visual.\n");
-        g_print("*** Trying single-buffered visual.\n");
-        /* Try single-buffered visual */
-        glconfig = gdk_gl_config_new_by_mode(static_cast<GdkGLConfigMode>(GDK_GL_MODE_RGB));
-        if (glconfig == NULL)
-        {
-            g_print ("*** No appropriate OpenGL-capable visual found.\n");
-            exit(1);
-        }
-    }
-    gltools::examine_gl_config_attrib(glconfig);
+    //GdkGLConfig* glconfig;
+    //// the line above does not work in C++ if the cast is not there.
+    //glconfig = gdk_gl_config_new_by_mode(static_cast<GdkGLConfigMode>(GDK_GL_MODE_RGB | GDK_GL_MODE_DOUBLE));
+    //if (glconfig == NULL)
+    //{
+    //    g_print("*** Cannot find the double-buffered visual.\n");
+    //    g_print("*** Trying single-buffered visual.\n");
+    //    /* Try single-buffered visual */
+    //    glconfig = gdk_gl_config_new_by_mode(static_cast<GdkGLConfigMode>(GDK_GL_MODE_RGB));
+    //    if (glconfig == NULL)
+    //    {
+    //        g_print ("*** No appropriate OpenGL-capable visual found.\n");
+    //        exit(1);
+    //    }
+    //}
+    //gltools::examine_gl_config_attrib(glconfig);
     // Main GTK window
     window_ = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_widget_set_size_request(window_, 640, 480);
@@ -282,18 +166,11 @@ Gui::Gui() :
     gtk_container_add(GTK_CONTAINER(window_), drawing_area_);
 
     //avoid flickering when resizing or obscuring the main window
-    //gtk_widget_realize(drawing_area_);
-    //gdk_window_set_back_pixmap(drawing_area_->window_, NULL, FALSE);
-    //gtk_widget_set_app_paintable(drawing_area_, TRUE);
-    //gtk_widget_set_double_buffered(drawing_area_, FALSE);
-
-    /* Set OpenGL-capability to the widget. */
-    gtk_widget_set_gl_capability(drawing_area_, glconfig, NULL, TRUE, GDK_GL_RGBA_TYPE);
+    gtk_widget_realize(drawing_area_);
+    gdk_window_set_back_pixmap(drawing_area_->window, NULL, FALSE);
+    gtk_widget_set_app_paintable(drawing_area_, TRUE);
+    gtk_widget_set_double_buffered(drawing_area_, FALSE);
   
-    g_signal_connect_after(G_OBJECT(drawing_area_), "realize", G_CALLBACK(on_realize), this);
-    g_signal_connect(G_OBJECT(drawing_area_), "configure_event", G_CALLBACK(on_configure_event), this);
-    g_signal_connect(G_OBJECT(drawing_area_), "expose_event", G_CALLBACK(on_expose_event), this);
-
     gtk_widget_show_all(window_);
 }
 
@@ -304,6 +181,8 @@ void run_gui(gint argc, gchar* argv[])
     gtk_gl_init(&argc, &argv);
     gst_init(&argc, &argv);
     Gui gui = Gui();
-    //Pipeline pipeline = Pipeline();
+    // init GST
+    Pipeline pipeline = Pipeline();
+    pipeline.set_drawing_area(gui.drawing_area_);
     gtk_main();
 }
--- src/pipeline.cpp
+++ src/pipeline.cpp
@@ -1,6 +1,11 @@
 #include <gtk/gtk.h>
 #include <GL/glx.h>
 #include <gst/gst.h>
+#include "pipeline.h"
+#include "draw.h"
+#include <gdk/gdk.h>
+#include "gstgtk.h"
+#include <iostream>
 
 /**
  * GST bus signal watch callback 
@@ -32,133 +37,201 @@ void Pipeline::end_stream_cb(GstBus* bus, GstMessage * msg, gpointer data)
     }
     gtk_main_quit();
 }
-
+void Pipeline::stop()
+{
+    gst_element_set_state(GST_ELEMENT(pipeline_), GST_STATE_NULL);
+    gst_object_unref(pipeline_);
+    // gtk main quit?
+}
 Pipeline::Pipeline()
 {
     pipeline_ = NULL;
-    state_ = 0;
     
+    pipeline_ = GST_PIPELINE(gst_pipeline_new("pipeline"));
+    //state_ = 0;
+    GstElement* glupload;
     
-    pipeline_ = GST_PIPELINE(gst_parse_launch("videotestsrc ! video/x-raw-yuv, width=320, height=240, framerate=(fraction)30/1 ! glupload ! fakesink sync=1", NULL));
+    videosrc_  = gst_element_factory_make("videotestsrc", "videotestsrc0");
+    glupload  = gst_element_factory_make ("glupload", "glupload0");
+    videosink_ = gst_element_factory_make("glimagesink", "glimagesink0");
+
+    if (!videosrc_ || !glupload || !videosink_)
+    {
+        g_print("one element could not be found \n");
+        exit(1);
+    }
+    //// change video source caps
+    //GstCaps *caps = gst_caps_new_simple("video/x-raw-yuv",
+    //                                    "width", G_TYPE_INT, 640,
+    //                                    "height", G_TYPE_INT, 480,
+    //                                    "framerate", GST_TYPE_FRACTION, 25, 1,
+    //                                    "format", GST_TYPE_FOURCC, GST_MAKE_FOURCC ('Y', 'U', 'Y', '2'),
+    //                                    NULL) ;
+    
+    /* change video source caps */
+    GstCaps *caps = gst_caps_new_simple("video/x-raw-rgb",
+                                        "width", G_TYPE_INT, 320,
+                                        "height", G_TYPE_INT, 240,
+                                        "framerate", GST_TYPE_FRACTION, 25, 1,
+                                        NULL) ;
+
+
+    GstCaps *outcaps = gst_caps_new_simple("video/x-raw-gl",
+                                        "width", G_TYPE_INT, 800,
+                                        "height", G_TYPE_INT, 600,
+                                        NULL) ;
+    
+    // configure elements
+    g_object_set(G_OBJECT(videosrc_), "num-buffers", 400, NULL);
+    g_object_set(G_OBJECT(videosink_), "client-reshape-callback", reshapeCallback, NULL);
+    g_object_set(G_OBJECT(videosink_), "client-draw-callback", drawCallback, NULL);
+    //g_object_set(G_OBJECT(videosink_), "client-data", NULL, NULL);
+    // add elements
+    gst_bin_add_many(GST_BIN(pipeline_), glupload, videosrc_, videosink_, NULL);
+    gboolean link_ok = gst_element_link_filtered(videosrc_, glupload, caps);
+    gst_caps_unref(caps);
+    if(!link_ok)
+    {
+        g_warning("Failed to link videosrc to glupload!\n") ;
+        exit(1);
+    }
+    link_ok = gst_element_link_filtered(glupload, videosink_, outcaps) ;
+    gst_caps_unref(outcaps) ;
+    if(!link_ok)
+    {
+        g_warning("Failed to link glupload to glimagesink!\n") ;
+        exit(1);
+    }
     /* setup bus */
-    bus = gst_pipeline_get_bus(GST_PIPELINE(pipeline_));
+    GstBus* bus = gst_pipeline_get_bus(GST_PIPELINE(pipeline_));
     gst_bus_add_signal_watch(bus);
     g_signal_connect(bus, "message::error", G_CALLBACK(end_stream_cb), this);
     g_signal_connect(bus, "message::warning", G_CALLBACK(end_stream_cb), this);
     g_signal_connect(bus, "message::eos", G_CALLBACK(end_stream_cb), this);
     gst_object_unref(bus);
-  
-}
 
-/**
- * Sets is an external OpenGL context with which gst-plugins-gl want to share textures 
- */
-void Pipeline::set_glx_context(GLXContext gl_context)
-{
-    static bool called = false;
-    GstElement* glupload0 = NULL;
-    GstBus* bus = NULL;
-    
-    if (! called)
+    // make it play !!
+
+    /* run */
+    GstStateChangeReturn ret;
+    ret = gst_element_set_state(GST_ELEMENT(pipeline_), GST_STATE_PLAYING);
+    if (ret == GST_STATE_CHANGE_FAILURE)
     {
-        called = true;
-    
-        glupload0 = gst_bin_get_by_name(GST_BIN(pipeline_), "glupload0");
-        g_object_set(G_OBJECT(glupload0), "external-opengl-context", gl_context, NULL);
-        g_object_unref(glupload0);
-        
-        /* NULL to PAUSED state pipeline to make sure the gst opengl context is created and
-         * shared with the other one 
-         */
-        gst_element_set_state(GST_ELEMENT(pipeline_), GST_STATE_PAUSED);
-        state_ = GST_STATE_PAUSED;
-        if (gst_element_get_state(GST_ELEMENT(pipeline_), &state_, NULL, GST_CLOCK_TIME_NONE) != GST_STATE_CHANGE_SUCCESS) 
+        g_print ("Failed to start up pipeline!\n");
+        /* check if there is an error message with details on the bus */
+        GstMessage* msg = gst_bus_poll (bus, GST_MESSAGE_ERROR, 0);
+        if (msg)
         {
-            g_debug("failed to pause pipeline\n");
-            exit(1);
+          GError *err = NULL;
+          gst_message_parse_error (msg, &err, NULL);
+          g_print ("ERROR: %s\n", err->message);
+          g_error_free (err);
+          gst_message_unref (msg);
         }
-        // set a callback to retrieve the gst gl textures
-        fakesink = gst_bin_get_by_name(GST_BIN(pipeline_), "fakesink0");
-        g_object_set(G_OBJECT(fakesink), "signal-handoffs", TRUE, NULL);
-        g_signal_connect(fakesink, "handoff", G_CALLBACK(on_gst_buffer), this);
-        g_object_unref(fakesink);
-
-        /* play gst */
-        gst_element_set_state (GST_ELEMENT (pipeline_), GST_STATE_PLAYING);
+        exit(1);
     }
 }
 
-/**
- * Called everytime there is a new video image ready to texture with.
- * fakesink handoff callback.
- *
- * @param element: The fakesink0 element.
- * @param buf: The pixels.
- * @param data: The this pointer.
- */
-static void on_gst_buffer(GstElement* element, GstBuffer* buf, GstPad * pad, gpointer data)
+gboolean on_expose_event(GtkWidget* widget, GdkEventExpose* event, GstElement* videosink)
 {
-    GAsyncQueue *queue_input_buf = NULL;
-    GAsyncQueue *queue_output_buf = NULL;
-    Pipeline *context = static_cast<Pipeline*>(data);
-  
-    // ref then push buffer to use it in clutter
-    gst_buffer_ref(buf);
-    queue_input_buf = g_object_get_data(G_OBJECT(texture_actor), "queue_input_buf");
-    g_async_queue_push(queue_input_buf, buf);
-    if (g_async_queue_length(queue_input_buf) > 2) 
-    {
-        clutter_threads_add_idle_full(G_PRIORITY_HIGH, update_texture_with_data, texture_actor, NULL);
-    }
-  
-    /* pop then unref buffer we have finished to use in clutter */
-    queue_output_buf = g_object_get_data(G_OBJECT(texture_actor), "queue_output_buf");
-    if (g_async_queue_length(queue_output_buf) > 2) 
-    {
-      GstBuffer *buf_old = g_async_queue_pop(queue_output_buf);
-      gst_buffer_unref(buf_old);
-    }
-}
-
-/* 
- * Updates an OpenGL texture with the data from the GST gl buffer
- */
-gboolean update_texture_with_data(gpointer data)
-{
-    ClutterTexture *texture_actor = (ClutterTexture *) data;
-    GAsyncQueue *queue_input_buf = g_object_get_data(G_OBJECT (texture_actor), "queue_input_buf");
-    GAsyncQueue *queue_output_buf = g_object_get_data(G_OBJECT (texture_actor), "queue_output_buf");
-    GstGLBuffer *gst_gl_buf = g_async_queue_pop(queue_input_buf);
-    ClutterActor *stage = g_object_get_data(G_OBJECT (texture_actor), "stage");
-    CoglHandle cogl_texture = 0;
-  
-    /* Create a cogl texture from the gst gl texture */
-    glEnable (GL_TEXTURE_RECTANGLE_ARB);
-    glBindTexture (GL_TEXTURE_RECTANGLE_ARB, gst_gl_buf->texture);
-    if (glGetError () != GL_NO_ERROR) 
-    {
-        g_debug ("failed to bind texture that comes from gst-gl\n");
-    }  
-    cogl_texture = cogl_texture_new_from_foreign (gst_gl_buf->texture,
-        CGL_TEXTURE_RECTANGLE_ARB, gst_gl_buf->width, gst_gl_buf->height, 0, 0,
-        COGL_PIXEL_FORMAT_RGBA_8888);
-    glBindTexture(GL_TEXTURE_RECTANGLE_ARB, 0);
-  
-    /* Previous cogl texture is replaced and so its ref counter discreases to 0.
-     * According to the source code, glDeleteTexture is not called when the previous
-     * ref counter of the previous cogl texture is reaching 0 because is_foreign is TRUE */
-    clutter_texture_set_cogl_texture (CLUTTER_TEXTURE (texture_actor), cogl_texture);
-    cogl_texture_unref(cogl_texture);
-  
-    /* we can now show the clutter scene if not yet visible */
-    if (!CLUTTER_ACTOR_IS_VISIBLE(stage))
-    {
-        clutter_actor_show_all(stage);
-    }
-  
-    /* push buffer so it can be unref later */
-    g_async_queue_push (queue_output_buf, gst_gl_buf);
+    gst_x_overlay_expose(GST_X_OVERLAY(videosink));
     return FALSE;
+}
+
+//client reshape callback
+void reshapeCallback(GLuint width, GLuint height, gpointer data)
+{
+    glViewport(0, 0, width, height);
+    
+    float w = float(width) / float(height);
+    float h = 1.0;
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-w, w, -h, h, -1.0, 1.0);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+gboolean drawCallback(GLuint texture, GLuint width, GLuint height, gpointer data)
+{
+    static GTimeVal current_time;
+    static glong last_sec = current_time.tv_sec;
+    static gint nbFrames = 0;
+
+    g_get_current_time(&current_time);
+    nbFrames++ ;
+
+    if ((current_time.tv_sec - last_sec) >= 1)
+    {
+        std::cout << "GRAPHIC FPS = " << nbFrames << std::endl;
+        nbFrames = 0;
+        last_sec = current_time.tv_sec;
+    }
+
+    // now, draw the texture
+    glEnable(GL_TEXTURE_RECTANGLE_ARB);
+    glBindTexture(GL_TEXTURE_RECTANGLE_ARB, texture);
+    glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glTranslatef(0.0f,0.0f,0.0f);
+
+    glBegin(GL_QUADS);
+	      // Front Face
+	      glTexCoord2f((gfloat)width / 2, 0.0f); glVertex3f(-1.0f, -1.0f,  1.0f);
+	      glTexCoord2f(0.0f, 0.0f); glVertex3f( 1.0f, -1.0f,  1.0f);
+	      glTexCoord2f(0.0f, (gfloat)height / 2 ); glVertex3f( 1.0f,  1.0f,  1.0f);
+	      glTexCoord2f((gfloat)width / 2, (gfloat)height / 2); glVertex3f(-1.0f,  1.0f,  1.0f);
+    glEnd();
+
+    // DRAW LINES
+    glDisable(GL_TEXTURE_RECTANGLE_ARB);
+    glColor4f(1.0, 1.0, 0.0, 0.6);
+    int num = 64;
+    float x;
+    for (int i = 0; i < num; i++)
+    {
+        x = (i / float(num)) * 4 - 2;
+        draw::draw_line(float(x), -2.0, float(x), 2.0);
+        draw::draw_line(-2.0, float(x), 2.0, float(x));
+    }
+    //return TRUE causes a postRedisplay
+    return TRUE;
+}
+
+GstBusSyncReply sync_handler(GstBus* bus, GstMessage* message, GtkWidget* widget)
+{
+    // ignore anything but 'prepare-xwindow-id' element messages
+    if (GST_MESSAGE_TYPE(message) != GST_MESSAGE_ELEMENT)
+    {
+        return GST_BUS_PASS;
+    }
+    if (!gst_structure_has_name(message->structure, "prepare-xwindow-id"))
+    {
+        return GST_BUS_PASS;
+    }
+    
+    g_print("setting xwindow id\n");
+    gst_x_overlay_set_gtk_window(GST_X_OVERLAY(GST_MESSAGE_SRC(message)), widget);
+    gst_message_unref(message);
+    return GST_BUS_DROP;
+}
+
+void Pipeline::set_drawing_area(GtkWidget* drawing_area)
+{
+    //needed when being in GST_STATE_READY, GST_STATE_PAUSED
+    //or resizing/obscuring the window
+    g_signal_connect(drawing_area, "expose-event", G_CALLBACK(on_expose_event), videosink_);
+    
+    GstBus* bus = gst_pipeline_get_bus(GST_PIPELINE(pipeline_));
+    gst_bus_set_sync_handler(bus, (GstBusSyncHandler)sync_handler, drawing_area);
+    gst_bus_add_signal_watch(bus);
 }
 
 Pipeline::~Pipeline() {}
