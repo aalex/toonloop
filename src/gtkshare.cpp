25a675626f663e716a196183b4833369c4e3ac56
Alexandre Quessy <alexandre@quessy.net>
2010-02-25 21:56:01 -0500

    towards a gtkglext + gst prototype.

--- src/gtkshare.cpp
+++ src/gtkshare.cpp
@@ -47,8 +47,7 @@ float rtri = 0.0f;
 float rquad = 0.0f;
 
 /* A general OpenGL initialization function.  Sets all of the initial parameters. */
-void
-InitGL (int Width, int Height)  // We call this right after our OpenGL window is created.
+void InitGL (int Width, int Height)  // We call this right after our OpenGL window is created.
 {
   glViewport (0, 0, Width, Height);
   glClearColor (0.0f, 0.0f, 0.0f, 0.0f);        // This Will Clear The Background Color To Black
@@ -66,8 +65,7 @@ InitGL (int Width, int Height)  // We call this right after our OpenGL window is
 }
 
 /* The main drawing function. */
-void
-DrawGLScene (GstGLBuffer * gst_gl_buf)
+void DrawGLScene (GstGLBuffer * gst_gl_buf)
 {
   GLuint texture = gst_gl_buf->texture;
   GLfloat width = (GLfloat) gst_gl_buf->width;
@@ -125,8 +123,7 @@ DrawGLScene (GstGLBuffer * gst_gl_buf)
   SDL_GL_SwapBuffers ();
 }
 
-gboolean
-update_gl_scene (void *fk)
+gboolean update_gl_scene (void *fk)
 {
   GstElement *fakesink = (GstElement *) fk;
   GAsyncQueue *queue_input_buf =
@@ -165,8 +162,7 @@ gboolean on_key_press_event(GtkWidget *widget, GdkEventKey *event, gpointer data
 }
 
 /* fakesink handoff callback */
-void
-on_gst_buffer (GstElement * fakesink, GstBuffer * buf, GstPad * pad,
+void on_gst_buffer (GstElement * fakesink, GstBuffer * buf, GstPad * pad,
     gpointer data)
 {
   GAsyncQueue *queue_input_buf = NULL;
@@ -192,8 +188,7 @@ on_gst_buffer (GstElement * fakesink, GstBuffer * buf, GstPad * pad,
 }
 
 /* gst bus signal watch callback */
-void
-end_stream_cb (GstBus * bus, GstMessage * msg, gpointer data)
+void end_stream_cb (GstBus * bus, GstMessage * msg, gpointer data)
 {
   switch (GST_MESSAGE_TYPE (msg)) {
 
@@ -227,14 +222,125 @@ end_stream_cb (GstBus * bus, GstMessage * msg, gpointer data)
   gtk_main_quit();
 }
 
-int
-main (int argc, char **argv)
+static void on_realize(GtkWidget *widget, gpointer data)
+{
+    GdkGLContext *glcontext = gtk_widget_get_gl_context (widget);
+    GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable (widget);
+  
+    /*** OpenGL BEGIN ***/
+    if (! gdk_gl_drawable_gl_begin(gldrawable, glcontext))
+        return;
+    
+    InitGL (widget->allocation.width, widget->allocation.height);  
+  
+    gdk_gl_drawable_gl_end (gldrawable);
+    /*** OpenGL END ***/
+}
+
+static gboolean on_configure_event(GtkWidget *widget, GdkEventConfigure *event, gpointer data)
+{
+    GdkGLContext *glcontext = gtk_widget_get_gl_context(widget);
+    GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable(widget);
+    /*** OpenGL BEGIN ***/
+    if (! gdk_gl_drawable_gl_begin(gldrawable, glcontext))
+    {
+        return FALSE;
+    }
+    InitGL (widget->allocation.width, widget->allocation.height);  
+    
+    gdk_gl_drawable_gl_end (gldrawable);
+    /*** OpenGL END ***/
+    return TRUE;
+}
+
+
+static gboolean on_expose_event (GtkWidget *widget, GdkEventExpose *event, gpointer data)
+{
+    GdkGLContext *glcontext = gtk_widget_get_gl_context (widget);
+    GdkGLDrawable *gldrawable = gtk_widget_get_gl_drawable (widget);
+    /*** OpenGL BEGIN ***/
+    if (!gdk_gl_drawable_gl_begin(gldrawable, glcontext))
+    {
+      return FALSE;
+    }
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // FIXME: draw stuff here.
+    glCallList(1);
+
+    if (gdk_gl_drawable_is_double_buffered(gldrawable))
+    {
+        gdk_gl_drawable_swap_buffers(gldrawable);
+    } else {
+        glFlush();
+    }
+    gdk_gl_drawable_gl_end(gldrawable);
+    /*** OpenGL END ***/
+    return TRUE;
+}
+
+static void print_gl_config_attrib(GdkGLConfig *glconfig, const gchar *attrib_str, int attrib, gboolean is_boolean)
+{
+    int value;
+    g_print ("%s = ", attrib_str);
+    if (gdk_gl_config_get_attrib(glconfig, attrib, &value))
+    {
+        if (is_boolean)
+        {
+            g_print("%s\n", value == TRUE ? "TRUE" : "FALSE");
+        } else {
+            g_print("%d\n", value);
+        } 
+    } else {
+        g_print("*** Cannot get %s attribute value\n", attrib_str);
+    }
+}
+
+static void examine_gl_config_attrib(GdkGLConfig *glconfig)
+{
+  g_print("\nOpenGL visual configurations :\n\n");
+  g_print("gdk_gl_config_is_rgba (glconfig) = %s\n", gdk_gl_config_is_rgba (glconfig) ? "TRUE" : "FALSE");
+  g_print("gdk_gl_config_is_double_buffered (glconfig) = %s\n", gdk_gl_config_is_double_buffered (glconfig) ? "TRUE" : "FALSE");
+  g_print("gdk_gl_config_is_stereo (glconfig) = %s\n", gdk_gl_config_is_stereo (glconfig) ? "TRUE" : "FALSE");
+  g_print("gdk_gl_config_has_alpha (glconfig) = %s\n", gdk_gl_config_has_alpha (glconfig) ? "TRUE" : "FALSE");
+  g_print("gdk_gl_config_has_depth_buffer (glconfig) = %s\n", gdk_gl_config_has_depth_buffer (glconfig) ? "TRUE" : "FALSE");
+  g_print("gdk_gl_config_has_stencil_buffer (glconfig) = %s\n", gdk_gl_config_has_stencil_buffer (glconfig) ? "TRUE" : "FALSE");
+  g_print("gdk_gl_config_has_accum_buffer (glconfig) = %s\n",
+  gdk_gl_config_has_accum_buffer (glconfig) ? "TRUE" : "FALSE");
+  g_print ("\n");
+  print_gl_config_attrib(glconfig, "GDK_GL_USE_GL", GDK_GL_USE_GL, TRUE);
+  print_gl_config_attrib(glconfig, "GDK_GL_BUFFER_SIZE", GDK_GL_BUFFER_SIZE, FALSE);
+  print_gl_config_attrib(glconfig, "GDK_GL_LEVEL", GDK_GL_LEVEL, FALSE);
+  print_gl_config_attrib(glconfig, "GDK_GL_RGBA", GDK_GL_RGBA, TRUE);
+  print_gl_config_attrib(glconfig, "GDK_GL_DOUBLEBUFFER", GDK_GL_DOUBLEBUFFER, TRUE);
+  print_gl_config_attrib(glconfig, "GDK_GL_STEREO", GDK_GL_STEREO, TRUE);
+  print_gl_config_attrib(glconfig, "GDK_GL_AUX_BUFFERS", GDK_GL_AUX_BUFFERS, FALSE);
+  print_gl_config_attrib(glconfig, "GDK_GL_RED_SIZE", GDK_GL_RED_SIZE, FALSE);
+  print_gl_config_attrib(glconfig, "GDK_GL_GREEN_SIZE", GDK_GL_GREEN_SIZE, FALSE);
+  print_gl_config_attrib(glconfig, "GDK_GL_BLUE_SIZE", GDK_GL_BLUE_SIZE, FALSE);
+  print_gl_config_attrib(glconfig, "GDK_GL_ALPHA_SIZE", GDK_GL_ALPHA_SIZE, FALSE);
+  print_gl_config_attrib(glconfig, "GDK_GL_DEPTH_SIZE", GDK_GL_DEPTH_SIZE, FALSE);
+  print_gl_config_attrib(glconfig, "GDK_GL_STENCIL_SIZE", GDK_GL_STENCIL_SIZE, FALSE);
+  print_gl_config_attrib(glconfig, "GDK_GL_ACCUM_RED_SIZE", GDK_GL_ACCUM_RED_SIZE, FALSE);
+  print_gl_config_attrib(glconfig, "GDK_GL_ACCUM_GREEN_SIZE", GDK_GL_ACCUM_GREEN_SIZE, FALSE);
+  print_gl_config_attrib(glconfig, "GDK_GL_ACCUM_BLUE_SIZE", GDK_GL_ACCUM_BLUE_SIZE, FALSE);
+  print_gl_config_attrib(glconfig, "GDK_GL_ACCUM_ALPHA_SIZE", GDK_GL_ACCUM_ALPHA_SIZE, FALSE);
+  g_print ("\n");
+}
+
+static void on_delete_event(GtkWidget* widget, GdkEvent* event, void* data)
+{
+    g_print("Close\n");
+    gtk_main_quit();
+}
+
+int main (int argc, char **argv)
 {
   SDL_SysWMinfo info;
   Display *gtkglext_display = NULL;
   Window gtkglext_window = 0;
   GLXContext gtkglext_gl_context = NULL;
-
+  
   GstPipeline *pipeline = NULL;
   GstBus *bus = NULL;
   GstElement *glfilter = NULL;
@@ -243,25 +349,61 @@ main (int argc, char **argv)
   GAsyncQueue *queue_input_buf = NULL;
   GAsyncQueue *queue_output_buf = NULL;
 
-  /* Initialize GTKGLEXT for video output */
-  if (SDL_Init (SDL_INIT_VIDEO) < 0) {
-    fprintf (stderr, "Unable to initialize SDL: %s\n", SDL_GetError ());
-    return -1;
-  }
-
-  /* Create a 640x480 OpenGL screen */
-  if (SDL_SetVideoMode (640, 480, 0, SDL_OPENGL) == NULL) {
-    fprintf (stderr, "Unable to create OpenGL screen: %s\n", SDL_GetError ());
-    SDL_Quit ();
-    return -1;
-  }
-
-
-  /* Loop, drawing and checking events */
-  InitGL (640, 480);
-
   gst_init (&argc, &argv);
   gtk_init (&argc, &argv);
+  gtk_gl_init(&argc, &argv);
+    
+  gint major; 
+  gint minor;
+  gdk_gl_query_version(&major, &minor);
+  g_print("\nOpenGL extension version - %d.%d\n", major, minor);
+  /* Try double-buffered visual */
+
+  GdkGLConfig* glconfig;
+  // the line above does not work in C++ if the cast is not there.
+  glconfig = gdk_gl_config_new_by_mode(static_cast<GdkGLConfigMode>(GDK_GL_MODE_RGB | GDK_GL_MODE_DOUBLE));
+  if (glconfig == NULL)
+  {
+      g_print("*** Cannot find the double-buffered visual.\n");
+      g_print("*** Trying single-buffered visual.\n");
+      /* Try single-buffered visual */
+      glconfig = gdk_gl_config_new_by_mode(static_cast<GdkGLConfigMode>(GDK_GL_MODE_RGB));
+      if (glconfig == NULL)
+      {
+          g_print ("*** No appropriate OpenGL-capable visual found.\n");
+          exit(1);
+      }
+  }
+  examine_gl_config_attrib(glconfig);
+
+  // Main GTK window
+  GtkWidget* window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_widget_set_size_request(window, 640, 480);
+  gtk_window_set_title(GTK_WINDOW (window), "Toonloop 1.3 experimental");
+  GdkGeometry geometry;
+  geometry.min_width = 1;
+  geometry.min_height = 1;
+  geometry.max_width = -1;
+  geometry.max_height = -1;
+  gtk_window_set_geometry_hints(GTK_WINDOW(window), window, &geometry, GDK_HINT_MIN_SIZE);
+  g_signal_connect(G_OBJECT(window), "delete-event", G_CALLBACK(on_delete_event), NULL);
+
+  //area where the video is drawn
+  GtkWidget* drawing_area = gtk_drawing_area_new();
+  gtk_container_add(GTK_CONTAINER(window), drawing_area);
+
+  /* Set OpenGL-capability to the widget. */
+  gtk_widget_set_gl_capability(drawing_area, glconfig, NULL, TRUE, GDK_GL_RGBA_TYPE);
+
+  /* Loop, drawing and checking events */
+  g_signal_connect_after(G_OBJECT(drawing_area), "realize", G_CALLBACK(on_realize), NULL);
+  g_signal_connect(G_OBJECT(drawing_area), "configure_event", G_CALLBACK(on_configure_event), NULL);
+  g_signal_connect(G_OBJECT(drawing_area), "expose_event", G_CALLBACK(on_expose_event), NULL);
+
+  gtk_widget_show_all(window);
+
+ // ------------------ done with the GTK GUI
+
 
   /* retrieve and turn off gtkglext opengl context */
   SDL_VERSION (&info.version);
