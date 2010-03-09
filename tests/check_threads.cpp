332b593df77097ed15d2e66eac847e515e893350
Alexandre Quessy <alexandre@quessy.net>
2010-03-08 20:33:50 -0500

    adding a thread prototype

---
+++ tests/check_threads.cpp
@@ -0,0 +1,23 @@
+// g++ -I/usr/include -L/usr/lib -lboost_thread-mt -o t1 check_threads.cpp
+#include <iostream>  
+#include <boost/thread.hpp>  
+#include <boost/date_time.hpp>  
+
+void run_worker()
+{
+    boost::posix_time::seconds workTime(1);  
+    std::cout << "Worker: running during one second..." << std::endl;  
+    boost::this_thread::sleep(workTime);  
+    std::cout << "Worker: finished" << std::endl;  
+}
+
+int main(int argc, char* argv[])  
+{  
+    std::cout << "main: startup" << std::endl;  
+    boost::thread workerThread(run_worker);  
+    std::cout << "main: waiting for thread" << std::endl;  
+    workerThread.join();  
+    std::cout << "main: done" << std::endl;  
+    return 0;  
+}
+
