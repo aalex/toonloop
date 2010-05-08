e5863bd8d1b5ec04897d35eb86d79b59950ce1f0
Alexandre Quessy <alexandre@quessy.net>
2010-05-08 15:30:11 -0400

    Done printing the ISO date and time.

--- tests/check_datetime.cpp
+++ tests/check_datetime.cpp
@@ -1,90 +1,28 @@
-/* The following is a simple example that shows conversion of dates 
- * to and from a std::string.
- * 
- * Expected output:
- * 2001-Oct-09
- * 2001-10-09
- * Tuesday October 9, 2001
- * An expected exception is next: 
- * Exception: Month number is out of range 1..12
+/* 
+ * Simply prints the current date time in the ISO format, with microseconds.
  */
-
-#include "boost/date_time/gregorian/gregorian.hpp"
+#include "boost/date_time/posix_time/posix_time.hpp"
 #include <iostream>
 #include <string>
 
+/**
+ * Converts the current local time to an ISO string in the form 
+ * YYYYMMDDTHHMMSS.fffffffff  where T is the date-time separator
+ */
+std::string get_now_as_iso_string()
+{
+    using namespace boost::posix_time;
+    //get the current time from the clock -- micro second resolution
+    ptime now = microsec_clock::local_time(); // posix_time::ptime
+    return to_iso_string(now);
+}
+
 int main() 
 {
-  using namespace boost::gregorian;
-
-    // Extended ISO format:
-    date d(2005, Jun, 25);
-    date_facet* facet(new date_facet("%Y-%m-%d %H:%M:%S%F%Q"));
-    //std::cout.imbue(std::locale(std::cout.getloc(), facet));
-    std::cout << d << std::endl;
-    //
-#if 0
-    try {
-
-
-        // The following date is in ISO 8601 extended format (CCYY-MM-DD)
-        std::string s("2001-10-9"); //2001-October-09
-        date d(from_simple_string(s));
-        std::cout << to_simple_string(d) << std::endl;
-
-
-        //Read ISO Standard(CCYYMMDD) and output ISO Extended
-        std::string ud("20011009"); //2001-Oct-09
-        date d1(from_undelimited_string(ud));
-        std::cout << to_iso_extended_string(d1) << std::endl;
-        
-        //Output the parts of the date - Tuesday October 9, 2001
-        date::ymd_type ymd = d1.year_month_day();
-        greg_weekday wd = d1.day_of_week();
-        std::cout << wd.as_long_string() << " "
-                  << ymd.month.as_long_string() << " "
-                  << ymd.day << ", " << ymd.year
-                  << std::endl;
-
-        //Let's send in month 25 by accident and create an exception
-        std::string bad_date("20012509"); //2001-??-09
-        std::cout << "An expected exception is next: " << std::endl;
-        date wont_construct(from_undelimited_string(bad_date));
-        //use wont_construct so compiler doesn't complain, but you wont get here!
-        std::cout << "oh oh, you shouldn't reach this line: " 
-                  << to_iso_string(wont_construct) << std::endl;
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << "Now = " << get_now_as_iso_string() << std::endl; 
     }
-    catch(std::exception& e) {
-        std::cout << "  Exception: " <<  e.what() << std::endl;
-    }
-#endif
     return 0;
 }
 
-# if 0 
-
-
-// date and time formatting prototype.
-
-using namespace boost::date_time;
-
-int main(int argc, char *argv[])
-{
-    // Extended ISO format:
-    //%Y-%m-%d %H:%M:%S%F%Q
-    date d(2005,Jun,25);
-    date_facet* facet(new date_facet("%A %B %d, %Y"));
-    std::cout.imbue(std::locale(std::cout.getloc(), facet));
-    std::cout << d << std::endl;
-    // "Saturday June 25, 2005"
-    return 0;
-}
-
-std::string to_iso_extended_string(ptime)
-
-    Convert to form YYYY-MM-DDTHH:MM:SS,fffffffff where T is the date-time separator
-
-    2002-01-31T10:00:01,123456789
-
-
-# endif
