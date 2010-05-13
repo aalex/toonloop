e96f9bf23e8ca9cbcbc591cbf14969a23d71f1d7
Alexandre Quessy <alexandre@quessy.net>
2010-05-13 01:35:39 -0400

    Figured out that I needed to link the check_symlinks with the boost::filesystem libraries explicitely.

--- tests/check_symlinks.cpp
+++ tests/check_symlinks.cpp
@@ -1,3 +1,4 @@
+// tests that we can make symlinks
 #include <iostream>
 #include "boost/filesystem.hpp"
 
@@ -14,7 +15,12 @@ int main(int argc, char **argv)
         std::cout << "creating directory" << std::endl;
         bool success = fs::create_directory(directory);
         if (!success)
+        {
             std::cout << "failed to create directory" << std::endl;
+            assert(success);
+        }
+    } else {
+        std::cout << "directory exists" << std::endl;
     }
 
     if (!fs::exists(to_p)) 
@@ -30,10 +36,20 @@ int main(int argc, char **argv)
             fs::create_symlink(to_p, from_p);
         } catch(const fs::filesystem_error &e) {
             std::cerr << "Error creating symlink: " << e.what() << std::endl;
+            assert(false);
         }
         //if (error_code != 0)
         //    std::cout << "ERROR!" << std::endl;
         std::cout << "Success!" << std::endl;
     }
+    try
+    {
+        std::cout << "remove directory tree" << std::endl;
+        fs::remove_all(directory);
+    } catch(const fs::filesystem_error &e) {
+        std::cerr << "Error removing directory tree: " << e.what() << std::endl;
+        assert(false);
+    }
     return 0;
 }
+
