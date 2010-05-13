27e4cf65c213d54ca6f5a4fb73d9d52e69049f54
Alexandre Quessy <alexandre@quessy.net>
2010-05-13 00:10:39 -0400

    made reading the output of check_tween easier

--- tests/check_tween.cpp
+++ tests/check_tween.cpp
@@ -9,7 +9,11 @@ int main(int argc, char *argv[])
     Tween chase[3];
     float res;
     float targets[3] ={ 1, 0, 1};
-    
+
+    if (VERBOSE)
+        std::cout << "TIME [0-100] | linear | inout | inout" << std::endl;
+    if (VERBOSE)
+        std::cout << "line up!" << std::endl;
     for (int i = 0; i < num_chases; i++)
     {
          chase[i] = Tween();
@@ -17,23 +21,27 @@ int main(int argc, char *argv[])
          chase[i].line(targets[i], 100);
     }
     // print 100 interpolated values
-    for (int t=0; t <= 100;t++)
+    for (int t=0; t <= 100; t++)
     {
         if (VERBOSE)
-            std::cout << "t:" << t << std::endl;
-        for (int i=0;i<num_chases;i++)
+            std::cout << "t:" << t << " ";
+        for (int i=0; i < num_chases; ++i)
         {
             // half way
             if (t == 50)
             {
+                if (VERBOSE)
+                    std::cout << "line down!" << std::endl;
                 chase[0].line(0.4, 0); // STOP
-                chase[1].line(0.25,100);
+                chase[1].line(0.25, 100);
             }
             // Tween_tick() should be renamed
-            res = chase[i].tick((float) t);
+            res = chase[i].tick(static_cast<float>(t));
             if (VERBOSE)
-                std::cout << " " << int(res * 255.0) << std::endl;
+                std::cout << " " << res << " ";
         }
+        if (VERBOSE)
+            std::cout << std::endl;
     }
     return 0;
 }
