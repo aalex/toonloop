30d9aeff53cb553c345b834fa9d7c7747f663874
Alexandre Quessy <alexandre@quessy.net>
2010-05-23 23:09:05 -0400

    first steps in adding OSC support

---
+++ src/oscreceiver.cpp
@@ -0,0 +1,69 @@
+#include "./oscreceiver.h"
+#include <iostream>
+#include <cstdio>
+
+OscReceiver::OscReceiver(const std::string &port) :
+    port_(port), 
+    server_(lo_server_thread_new(port_.c_str(), error))
+{
+    /* add method that will match any path and args */
+    lo_server_thread_add_method(server_, NULL, NULL, genericHandler, this);
+}
+
+
+OscReceiver::~OscReceiver()
+{
+    std::cout << "Freeing server thread\n";
+    lo_server_thread_free(server_);
+}
+
+
+void OscReceiver::addHandler(const char *path, const char *types, lo_method_handler handler, void *userData)
+{
+    lo_server_thread_add_method(server_, path, types, handler, userData);
+}
+
+void OscReceiver::listen()
+{
+    std::cout << "Listening on port " << port_ << std::endl;
+    lo_server_thread_start(server_);
+
+}
+
+
+void OscReceiver::error(int num, const char *msg, const char *path)
+{
+    std::cerr << "liblo server error " << num << " in path " << path 
+        << ": " << msg << std::endl;
+}
+
+/* catch any incoming messages and display them. returning 1 means that the 
+ *  * message has not been fully handled and the server should try other methods */
+int OscReceiver::genericHandler(const char *path, 
+        const char *types, lo_arg **argv, 
+        int argc, void *data, void *user_data) 
+{ 
+    //OscReceiver *context = static_cast<OscReceiver*>(user_data);
+    int i; 
+
+#ifdef CONFIG_DEBUG
+    printf("path: <%s>\n", path); 
+    for (i = 0; i < argc; ++i) 
+    { 
+        printf("arg %d '%c' ", i, types[i]); 
+        lo_arg_pp(static_cast<lo_type>(types[i]), argv[i]); 
+        printf("\n"); 
+    } 
+    printf("\n"); 
+    fflush(stdout); 
+#endif CONFIG_DEBUG
+
+    return 1; 
+} 
+
+
+std::string OscReceiver::toString() const
+{
+    return "port:" + port_;
+}
+
---
+++ src/oscreceiver.h
@@ -0,0 +1,27 @@
+#ifndef _OSC_RECEIVER_H_
+#define _OSC_RECEIVER_H_
+
+#include "lo/lo.h"
+#include <string>
+
+class OscReceiver {
+    public:
+        OscReceiver(const std::string &port);
+        ~OscReceiver();
+        void listen();
+        const char * port() const { return port_.c_str(); }
+        void addHandler(const char *path, 
+                const char *types, lo_method_handler handler, 
+                void *user_data);
+    private:
+        std::string toString() const;
+        std::string port_;
+        lo_server_thread server_;
+        static int genericHandler(const char *path, 
+                const char *types, lo_arg **argv, 
+                int argc, void *data, void *user_data);
+
+        static void error(int num, const char *msg, const char *path);
+};
+
+#endif // _OSC_RECEIVER_H_
