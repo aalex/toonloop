ad1953f5b54c4c39722be12c0b855a14f651009a
Alexandre Quessy <alexandre@quessy.net>
2010-08-16 03:39:32 -0400

    Towards using members functions with boost signals
    By the way, Ardour dropped sigc++ for boost::signals2

--- tests/check_signals.cpp
+++ tests/check_signals.cpp
@@ -17,6 +17,18 @@ void print_sum(float x, float y)
     std::cout << x << " + " << y << " = " << x + y << std::endl;
 }
 
+class DummyObject 
+{
+    public:
+        void foo(int i);
+};
+void DummyObject::foo(int i)
+{
+    std::cout << "foo(" << i <<  ");" << std::endl;
+}
+/**
+ * Simple example with no argument
+ */
 void test_a()
 {
 
@@ -26,7 +38,9 @@ void test_a()
     std::cout << "Calling the signal A." << std::endl;
     sig_a();
 }
-
+/**
+ * Passing float arguments
+ */
 void test_b()
 {
 
@@ -35,10 +49,25 @@ void test_b()
     std::cout << "Calling the signal B." << std::endl;
     sig_b(3.14159, 1.618);
 }
+
+
+///** 
+// * Using an object's method.
+// */
+//void test_c()
+//{
+//    DummyObject obj;
+//    boost::signal<void (int)> sig_c;
+//    sig_c.connect(&obj.foo);
+//    std::cout << "Calling the signal C." << std::endl;
+//    sig_c(32768);
+//}
+
 int main(int argc, char **argv)
 {
     test_a();
     test_b();
+//    test_c();
     return 0;
 }
 
