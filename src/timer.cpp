ba23d616e69535a7fe9ecc2c008ca71b6bed4d61
Alexandre Quessy <alexandre@quessy.net>
2010-08-16 22:06:11 -0400

    adding the licence to a few files

--- src/timer.cpp
+++ src/timer.cpp
@@ -1,11 +1,28 @@
-/* 
- * Timer class
+/*
+ * Toonloop
+ *
+ * Copyright 2010 Alexandre Quessy
+ * <alexandre@quessy.net>
+ * http://www.toonloop.com
+ *
+ * Toonloop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Toonloop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the gnu general public license
+ * along with Toonloop.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "timing.h"
 #include "timer.h"
 
 /** 
- * Float timer
+ * Timer using floats
  */
 Timer::Timer()
 {
