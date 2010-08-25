69ea7f7c5360b353c8c22ea45766a391027841aa
Alexandre Quessy <alexandre@quessy.net>
2010-08-25 10:25:40 -0400

    adding controller.cpp

---
+++ src/controller.cpp
@@ -0,0 +1,47 @@
+/*
+ * Toonloop
+ *
+ * Copyright 2010 Alexandre Quessy
+ * <alexandre@quessy.net>
+ * http://www.toonloop.com
+ *
+ * Toonloop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Toonloop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the gnu general public license
+ * along with Toonloop.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <boost/bind.hpp>
+#include <boost/signals2.hpp>
+#include "application.h"
+#include "controller.h"
+
+Controller::Controller(Application* owner) : 
+    owner_(owner)
+{
+    
+}
+
+void Controller::add_frame()
+{
+
+}
+void Controller::remove_frame()
+{
+
+}
+void Controller::choose_clip(int i)
+{
+
+}
+void Controller::save_clip(int i)
+{
+
+}
---
+++ src/controller.h
@@ -0,0 +1,88 @@
+/*
+ * Toonloop
+ *
+ * Copyright 2010 Alexandre Quessy
+ * <alexandre@quessy.net>
+ * http://www.toonloop.com
+ *
+ * Toonloop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Toonloop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the gnu general public license
+ * along with Toonloop.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef __CONTROLLER_H__
+#define __CONTROLLER_H__
+
+#include <boost/bind.hpp>
+#include <boost/signals2.hpp>
+#include "application.h"
+
+namespace s2 = boost::signals2;
+/**
+ * The Controller contains the methods that any class should call
+ * in order to create Toonloop animations. It also contains the signals 
+ * to which they should connect their slots in order to subscribe to 
+ * the events notifications. 
+ */
+class Controller
+{
+    public:
+        /**
+         * Constructor
+         */
+        Controller(Application* owner);
+        /** 
+         * Called when a frame is added to a clip.
+         * Arguments: clip number, new frame number.
+         */
+        s2::signal<void (int, int)> add_frame_signal_;
+        /** 
+         * Called when a frame is removed from a clip.
+         * Arguments: clip number, deleted frame number.
+         */
+        s2::signal<void (int, int)> remove_frame_signal_;
+        /** 
+         * Called when a clip is chosen.
+         * Arguments: clip number.
+         */
+        s2::signal<void (int)> choose_clip_signal_;
+        /** 
+         * Called when the FPS of a clip changes.
+         * Arguments: clip number, FPS.
+         */
+        s2::signal<void (int, int)> clip_fps_changed_signal_;
+        /** 
+         * Called when a clip is saved.
+         * Arguments: clip number.
+         */
+        s2::signal<void (int)> save_clip_signal_;
+        /**
+         * Adds a frame to the current clip.
+         */
+        void add_frame();
+        /**
+         * Removes a frame from the current clip.
+         */
+        void remove_frame();
+        /** 
+         * Chooses a clip
+         */
+        void choose_clip(int i);
+        /** 
+         * Saves a clip
+         */
+        void save_clip(int i);
+
+    private:
+        Application* owner_;
+};
+
+#endif 
