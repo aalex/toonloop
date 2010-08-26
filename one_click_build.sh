cf6315fdd0a002e07ea57f82c56cd77477ad55e7
Alexandre Quessy <alexandre@quessy.net>
2010-08-26 01:29:42 -0400

    updated one_click_build

--- one_click_build.sh
+++ one_click_build.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-# to use:
-# sudo apt-get install libnotify-bin
 
 cd "`dirname $BASH_SOURCE`"
-#notify-send -t 2000 "Building boomers..."
 ./autogen.sh
 ./configure
 make -j
-#gksu make install 
-#notify-send -t 10000 "Done building boomers !"
 
