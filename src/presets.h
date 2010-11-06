dea9bcc2fb798495b1cd652e288a4771cba358c3
Alexandre Quessy <alexandre@quessy.net>
2010-11-06 00:04:36 -0400

    added get_rules &

--- src/presets.h
+++ src/presets.h
@@ -1,3 +1,23 @@
+/*
+ * Toonloop
+ *
+ * Copyright 2010 Alexandre Quessy
+ * <alexandre@quessy.net>
+ * http://www.toonloop.com
+ *
+ * Toonloop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Toonloop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the gnu general public license
+ * along with Toonloop.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef __PRESETS_H__
 #define __PRESETS_H__
 
@@ -27,7 +47,7 @@ class MidiBinder
         MidiBinder();
         bool load_xml_file(const gchar *file_name);
         //MidiBinding *current_binding;
-        MidiRule *get_rule(const gchar *name);
+        //MidiRule *get_rule(const gchar *name);
         //void add_rule(const gchar *name);
         //void add_attribute_to_rule(const gchar *rule, const gchar *attr, const gchar *value);
         static void on_midi_xml_start_element(
@@ -37,6 +57,7 @@ class MidiBinder
             const gchar **attribute_values,
             gpointer user_data,
             GError **error);
+        const std::vector<MidiRule> &get_rules() { return rules_; }
     private:
         std::vector<MidiRule> rules_;
 };
