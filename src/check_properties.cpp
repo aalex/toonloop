e73a2783fbc6121ad85a919452e2efc4c8a6aefa
Alexandre Quessy <alexandre@quessy.net>
2010-11-13 02:52:24 -0500

    testing bool properties

--- src/check_properties.cpp
+++ src/check_properties.cpp
@@ -4,17 +4,22 @@
 #include <map>
 #include <iostream>
 
-void cb(std::string &name, int value)
+void int_cb(std::string &name, int value)
 {
     std::cout << name << " changed to " << value << std::endl;
 }
 
-void f_cb(std::string &name, float value)
+void float_cb(std::string &name, float value)
 {
     std::cout << name << " changed to " << value << std::endl;
 }
 
-void s_cb(std::string &name, std::string value)
+void string_cb(std::string &name, std::string value)
+{
+    std::cout << name << " changed to " << value << std::endl;
+}
+
+void bool_cb(std::string &name, bool value)
 {
     std::cout << name << " changed to " << value << std::endl;
 }
@@ -23,13 +28,13 @@ int main(int /* argc */, char ** /*argv */)
 {
     std::string name("hello");
     Property<int> p(name, 0);
-    p.value_changed_signal_.connect(&cb);
+    p.value_changed_signal_.connect(&int_cb);
     p.set_value(8);
 
     Properties<int> holder = Properties<int>();
     holder.add_property("bar", 9);
     Property<int> *x = holder.get_property("bar");
-    x->value_changed_signal_.connect(&cb);
+    x->value_changed_signal_.connect(&int_cb);
     //x->register_on_changed_slot(&cb);
     x->set_value(6);
     holder.remove_property("bar");
@@ -48,12 +53,16 @@ int main(int /* argc */, char ** /*argv */)
         return 1;
 
     Property<float> f("float property", 0.0);
-    f.value_changed_signal_.connect(&f_cb);
+    f.value_changed_signal_.connect(&float_cb);
     f.set_value(3.14159);
 
     Property<std::string> s("string property", "");
-    s.value_changed_signal_.connect(&s_cb);
-    s.set_value("boo!");
+    s.value_changed_signal_.connect(&string_cb);
+    s.set_value("some string");
+
+    Property<bool> b("bool property", false);
+    b.value_changed_signal_.connect(&bool_cb);
+    b.set_value(true);
 
     return 0;
 }
