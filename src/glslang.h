8a0df251949d92cb6a00998e18b684b59598d484
Alexandre Quessy <alexandre@quessy.net>
2010-12-13 15:16:06 -0500

    Updating the list of copyright holders

--- src/glslang.h
+++ src/glslang.h
@@ -1,3 +1,23 @@
+/*
+ * Toonloop
+ *
+ * Copyright (c) 2010 Alexandre Quessy <alexandre@quessy.net>
+ * Copyright (c) 2010 Tristan Matthews <le.businessman@gmail.com>
+ *
+ * Toonloop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Toonloop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the gnu general public license
+ * along with Toonloop.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __GLSLANG_H__
 #define __GLSLANG_H__
 #include <clutter/clutter.h>
