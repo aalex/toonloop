25331b3fc61687bb76ad03e6ea69301b84cdaa7f
Alexandre Quessy <alexandre@quessy.net>
2011-06-24 10:52:40 -0400

    add legacyclutter.h

---
+++ src/legacyclutter.h
@@ -0,0 +1,71 @@
+/*
+ * Toonloop
+ *
+ * Copyright (c) 2010 Alexandre Quessy <alexandre@quessy.net>
+ * Copyright (c) 2010 Tristan Matthews <le.businessman@gmail.com>
+ *
+ * Toonloop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Toonloop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the gnu general public license
+ * along with Toonloop.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __LEGACYCLUTTER_H__
+#define __LEGACYCLUTTER_H__
+
+#if CLUTTER_CHECK_VERSION(1,6,0)
+#else
+#define CLUTTER_KEY_0 CLUTTER_0
+#define CLUTTER_KEY_1 CLUTTER_1
+#define CLUTTER_KEY_2 CLUTTER_2
+#define CLUTTER_KEY_3 CLUTTER_3
+#define CLUTTER_KEY_4 CLUTTER_4
+#define CLUTTER_KEY_5 CLUTTER_5
+#define CLUTTER_KEY_6 CLUTTER_6
+#define CLUTTER_KEY_7 CLUTTER_7
+#define CLUTTER_KEY_8 CLUTTER_8
+#define CLUTTER_KEY_9 CLUTTER_9
+#define CLUTTER_KEY_BackSpace CLUTTER_BackSpace
+#define CLUTTER_KEY_Caps_Lock CLUTTER_Caps_Lock
+#define CLUTTER_KEY_Down CLUTTER_Down
+#define CLUTTER_KEY_Escape CLUTTER_Escape
+#define CLUTTER_KEY_F1 CLUTTER_F1
+#define CLUTTER_KEY_F2 CLUTTER_F2
+#define CLUTTER_KEY_Left CLUTTER_Left
+#define CLUTTER_KEY_Page_Down CLUTTER_Page_Down
+#define CLUTTER_KEY_Page_Up CLUTTER_Page_Up
+#define CLUTTER_KEY_Return CLUTTER_Return
+#define CLUTTER_KEY_Right CLUTTER_Right
+#define CLUTTER_KEY_Tab CLUTTER_Tab
+#define CLUTTER_KEY_Up CLUTTER_Up
+#define CLUTTER_KEY_a CLUTTER_a
+#define CLUTTER_KEY_b CLUTTER_b
+#define CLUTTER_KEY_bracketleft CLUTTER_bracketleft
+#define CLUTTER_KEY_bracketright CLUTTER_bracketright
+#define CLUTTER_KEY_e CLUTTER_e
+#define CLUTTER_KEY_f CLUTTER_f
+#define CLUTTER_KEY_i CLUTTER_i
+#define CLUTTER_KEY_j CLUTTER_j
+#define CLUTTER_KEY_k CLUTTER_k
+#define CLUTTER_KEY_o CLUTTER_o
+#define CLUTTER_KEY_parenleft CLUTTER_parenleft
+#define CLUTTER_KEY_parenright CLUTTER_parenright
+#define CLUTTER_KEY_period CLUTTER_period
+#define CLUTTER_KEY_q CLUTTER_q
+#define CLUTTER_KEY_r CLUTTER_r
+#define CLUTTER_KEY_s CLUTTER_s
+#define CLUTTER_KEY_semicolon CLUTTER_semicolon
+#define CLUTTER_KEY_space CLUTTER_space
+#define CLUTTER_KEY_x CLUTTER_x
+#endif
+
+#endif // __LEGACYCLUTTER_H__
+
