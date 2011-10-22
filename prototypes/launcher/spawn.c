2ebc5018db8f5681846f9cafcde5f986f907625d
Alexandre Quessy <alexandre@quessy.net>
2011-10-22 19:01:48 -0400

    use g_spawn_async_with_pipes

--- prototypes/launcher/spawn.c
+++ prototypes/launcher/spawn.c
@@ -3,37 +3,46 @@
  * gcc -Wall `pkg-config --libs --cflags glib-2.0` -o spawn spawn.c
  */
 #include <glib.h>
-#include <stdio.h>
+//#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+static gint std_output;
 
 static void run(void)
 {
     GError *err;
-    gchar *output = NULL;
     err = NULL;
-    if (! g_spawn_command_line_sync(
-          "/bin/sh -c 'echo hello'",
-          &output, NULL, NULL, &err))
+    gchar *args[] = {
+        "sh",
+        "-c",
+        "echo hello",
+        NULL
+        };
+    GSpawnFlags flags = G_SPAWN_SEARCH_PATH;
+    GPid pid;
+    if (! g_spawn_async_with_pipes(
+        NULL, /* inherit working directory */
+        args,
+        NULL, /* inherit env vars */
+        flags,
+        NULL, /* function to call after fork(), before exec() */
+        NULL, /* user data */
+        &pid,
+        NULL,
+        &std_output,
+        NULL,
+        &err))
     {
-        fprintf(stderr, "Error: %s\n", err->message);
+        g_warning("Error: %s\n", err->message);
         g_error_free(err);
         exit(1);
     }
     else
     {
-        g_assert(output != NULL);
-        if (strcmp(output, "hello\n") != 0)
-        {
-            printf("output was '%s', should have been 'hello'\n", output);
-            exit(1);
-        }
-        else
-        {
-            printf("output was '%s': good\n", output);
-        }
-        g_free(output);
+        g_print("success\n");    
     }
+    /** TODO: close std_output socket */
 }
 
 int main(int argc, char *argv[])
